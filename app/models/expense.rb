92b551f8d2b7ebb871ff5bb8c526568b12c57271
EroAuditore <alfa.fenrir@gmail.com>
2021-05-26 19:57:45 -0500

    validate presence

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,6 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User' #:user
   belongs_to :group, optional: true
   scope :external, -> { where(group_id: nil) }
+  validates :amount, numericality: { only_integer: true }
+  validates :name, presence: true
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :expenses
   has_one_attached :image
+  validates :name, presence: true
 end
